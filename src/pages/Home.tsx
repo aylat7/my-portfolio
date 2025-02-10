038ca3dab8a4cfd636a7c56c462a42035d830eaf
Ayla Topuz <ayla.topuz2005@gmail.com>
2025-02-10 02:19:32 -0500

    Move headshot to public folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -110,8 +110,8 @@ const Home = () => {
                 }}
               />
               <img
-                src="/src/assets/images/headshot.jpg"
-                alt="Profile"
+                src="/public/images/headshot.jpg"
+                alt="Ayla Topuz"
                 className="absolute inset-0 w-full h-full object-cover rounded-lg border-4 border-white shadow-xl"
               />
             </motion.div>
